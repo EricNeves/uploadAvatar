de4848e95852cd814790492f7f4e9d73358e599d
Eric Neves <ericnevesrodrigues@gmail.com>
2023-07-31 10:40:22 -0400

    Feat: update database.sql

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ create database if not exists upload;
 use upload;
 
 create table if not exists avatar (
-	id int unsigned not null auto_increment,
+  id int unsigned not null auto_increment,
   avatar longblob not null,
   img_type varchar(12),
   primary key (id)
